29ed4cfc348cbd22f8c66f5e6a3e21e5681752cd
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-08-05 14:39:33 +0530

    Added base boiler plate for react project , and webpack configuration.

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { createStore ,applyMiddleware } from "redux";
+import
+
+export default class App extends Component {
+  render() {
+    return <div>Hello world! test</div>;
+  }
+}
